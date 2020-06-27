ec47307ba84dcc99fc0a6e9410a5402c23f03f6c
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-06-26 19:36:40 -0500

    add useEffect cleanup function

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -7,17 +7,37 @@ export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
 
   useEffect(function () {
-    window.fetch('https://petgram.daneliasanchez20.vercel.app/categories')
+    window.fetch('https://petgram-server-danelia.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
       })
   }, [])
-  return (
-    <List>
+
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }

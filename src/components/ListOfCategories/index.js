1520886b9ca6d8f3f756d2ac1ac5c24088bfce7e
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-06-26 23:09:31 -0500

    add custom hook

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,18 +2,26 @@ import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-
-export const ListOfCategories = () => {
+import { Loader } from '../Loader'
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(function () {
+    setLoading(true)
     window.fetch('https://petgram-server-danelia.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])
 
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
   const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
@@ -28,12 +36,15 @@ export const ListOfCategories = () => {
   }, [showFixed])
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
+    <List fixed={fixed}>
       {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        loading
+          ? <Item key='loading'><Loader /></Item>
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
+
   return (
     <>
       {renderList()}

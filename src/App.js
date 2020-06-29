2f6197a8b9a93931d0d8d2551a1fde783004cc48
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-06-28 23:04:27 -0500

    add NavBar component and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Router } from '@reach/router'
@@ -12,9 +13,11 @@ export const App = () => {
       <Logo />
       <Router>
         <Home path='/' />
+        <Home path='/pet/:categoryId' />
         <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
       </Router>
-      <Detail path='/detail/:detailId' />
+      <NavBar />
     </div>
   )
 }
